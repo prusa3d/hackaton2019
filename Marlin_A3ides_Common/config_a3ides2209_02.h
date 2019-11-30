68bc8424a96fd78eb9192e693a63b5d3f599a31b
Vítězslav Šimek <vita.simek@gmail.com>
2019-11-30 10:21:03 +0100

    Enable serial output though USB.

--- Marlin_A3ides_Common/config_a3ides2209_02.h
+++ Marlin_A3ides_Common/config_a3ides2209_02.h
@@ -7,9 +7,9 @@
 //DBG - debug/trace configuration
 #define DBG_RTOS       // use FreeRTOS (semaphore and osDelay instead of HAL_Delay)
 #ifdef _DEBUG
-#define DBG_SWO        // trace to swo port
+//#define DBG_SWO        // trace to swo port
 //#define DBG_UART     6 // trace to uart6 port
-//#define DBG_CDC        // trace to cdc port
+#define DBG_CDC        // trace to cdc port
 #define DBG_LEVEL    1 // debug level (0..3)
 #else
 //#define DBG_SWO        // trace to swo port
